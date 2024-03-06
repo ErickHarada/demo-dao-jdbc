2fadf489fc0c225b28288174cae19cdbc9035a0c
Erick <eefeaga2000@gmail.com>
2024-03-06 18:55:07 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,15 @@
 package application;
 
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+
+        Department d = new Department(1, "Books");
+        Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, d);
+        System.out.println(seller);
     }
 }
