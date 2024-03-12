9898e0860e804feb8750a5b31abdcaff6afe4516
Erick <eefeaga2000@gmail.com>
2024-03-12 19:12:24 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.SellerDao;
+import model.dao.factory.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,7 @@ public class Main {
 
         Department d = new Department(1, "Books");
         Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, d);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
